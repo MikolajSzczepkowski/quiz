c32b959ef025b5b2248d060e1e3e2fbc9adfabb9
Mikołaj Szczepkowski <mikolajszczepkowski@gmail.com>
2015-06-02 20:52:02 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var allQuestions = [{question: "1. Who is Prime Minister of the United Kingdom?", 
+					choices: ["1.David Cameron", "1.Gordon Brown", "1.Winston Churchill", "1.Tony Blair"], correctAnswer:0},
+					{question: "2. Who is Prime Minister of the United Kingdom?", 
+					choices: ["2.David Cameron", "2.Gordon Brown", "2.Winston Churchill", "2.Tony Blair"], correctAnswer:1},
+					{question: "3. Who is Prime Minister of the United Kingdom?", 
+					choices: ["3.David Cameron", "3.Gordon Brown", "3.Winston Churchill", "3.Tony Blair"], correctAnswer:2},
+					{question: "4. Who is Prime Minister of the United Kingdom?", 
+					choices: ["4.David Cameron", "4.Gordon Brown", "4.Winston Churchill", "4.Tony Blair"], correctAnswer:3},
+					];
+var score = 0;
